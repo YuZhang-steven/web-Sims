cd9e4f09a41d88a1044b2fa8b5c0d015dacb7c39
S.Zhang <zengeryounke@hotmail.com>
2024-02-12 14:35:05 -0800

    instanced model show

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,13 +1,17 @@
 import { Environment } from "@react-three/drei";
 import { AnimatedWoman } from "./assets/AnimatedWoman";
+import { AnimatedWomanIns, Model } from "./assets/AnimatedWomanIns";
 
 export function Experience() {
     return (
         <>
             <Environment preset="sunset" />
             <ambientLight intensity={0.3} />
-            <AnimatedWoman />
-            <AnimatedWoman position-x={1} hairColor="red" topColor="cyan" />
+            <AnimatedWomanIns >
+                <Model />
+            </AnimatedWomanIns>
+            {/* <AnimatedWoman />
+            <AnimatedWoman position-x={1} hairColor="red" topColor="cyan" /> */}
         </>
     )
 }
