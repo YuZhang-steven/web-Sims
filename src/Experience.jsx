c9cd3367c7989beb2a134006b4628d3810cd76c9
S.Zhang <zengeryounke@hotmail.com>
2024-02-14 17:40:02 -0800

    skeleton clone

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,14 @@
 import { Environment } from "@react-three/drei";
 import { AnimatedWoman } from "./assets/AnimatedWoman";
-import { AnimatedWomanIns, Model } from "./assets/AnimatedWomanIns";
 
 export function Experience() {
     return (
         <>
             <Environment preset="sunset" />
             <ambientLight intensity={0.3} />
-            <AnimatedWomanIns >
-                <Model />
-            </AnimatedWomanIns>
 
-            <AnimatedWoman />
-            {/* <AnimatedWoman position-x={1} hairColor="red" topColor="cyan" /> */}
-        </>
-    )
+            <AnimatedWoman /> */
+/* <AnimatedWoman position-x={1} hairColor="red" topColor="cyan" />
+</>
+)
 }
