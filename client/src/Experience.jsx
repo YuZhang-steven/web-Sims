02a8c6879f8eebc7fce77473a64db24b13ae08ac
S.Zhang <zengeryounke@hotmail.com>
2024-02-15 12:10:10 -0800

    character click to change postions

--- client/src/Experience.jsx
+++ client/src/Experience.jsx
@@ -1,19 +1,33 @@
 import { useAtom, atom } from "jotai"
 
 
-import { ContactShadows, Environment } from "@react-three/drei";
+import { ContactShadows, Environment, useCursor } from "@react-three/drei";
 import { AnimatedWoman } from "./assets/AnimatedWoman";
-import { charactersAtom } from "./components/SocketManager";
+import { charactersAtom, socket } from "./components/SocketManager";
+import { useState } from "react";
 
 
 export function Experience() {
     const [characters] = useAtom(charactersAtom)
+    const [onFoor, setOnFloor] = useState(false)
+    useCursor(onFoor)
 
     return (
         <>
             <Environment preset="sunset" />
             <ambientLight intensity={0.3} />
             <ContactShadows blur={2.5} />
+
+            <mesh
+                rotation-x={-Math.PI / 2}
+                position-y={-0.001}
+                onClick={(e) => socket.emit("move", [e.point.x, 0, e.point.z])}
+                onPointerEnter={() => setOnFloor(true)}
+                onPointerLeave={() => setOnFloor(false)}
+            >
+                <planeGeometry args={[10, 10]} />
+                <meshStandardMaterial color={"#f0f0f0"} />
+            </mesh>
 
             {
                 characters.map((character) => (
