af3cb0acc33302a4b5e78ce0135141ce800ada6e
S.Zhang <zengeryounke@hotmail.com>
2024-02-15 00:07:16 -0800

    add server socket.io

---
+++ server/index.js
@@ -0,0 +1,20 @@
+
+import { Server, Socket } from "socket.io";
+
+const io = new Server({
+    cors: {
+        origin: "http://localhost:5173/"
+    }
+})
+
+io.listen(3001);
+
+io.on("connection", (socket) => {
+    console.log("user connected")
+
+    socket.emit("hello")
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+    })
+})
