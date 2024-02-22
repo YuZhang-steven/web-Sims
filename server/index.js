8a3aae1f3e01341175411756c490a1d4aeb52b5d
S.Zhang <zengeryounke@hotmail.com>
2024-02-22 00:59:17 -0800

    a star path finding with grid

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 
+import pathfinding from "pathfinding";
 import { Server } from "socket.io";
+// import { Item } from "../client/src/components/item";
 /**
  * initialize server
  */
@@ -424,6 +426,51 @@ const map = {
         },
     ],
 };
+
+/**
+ * PathFinding
+ */
+
+const grid = new pathfinding.Grid(map.size[0] * map.gridDivision, map.size[1] * map.gridDivision)
+
+const finder = new pathfinding.AStarFinder(
+    {
+        allowDiagnoal: true,
+        dontCrossCorners: true,
+    }
+)
+
+const findPath = (start, end) => {
+
+    const gridClone = grid.clone()
+    const path = finder.findPath(start[0], start[1], end[0], end[1], gridClone)
+    return path
+}
+
+const updateGrid = () => {
+    map.items.forEach((item) => {
+        if (item.walkable || item.wall) {
+            return
+        }
+
+        const width = item.rotation === 1 || item.rotation === 3 ? item.size[1] : item.size[0]
+        const height = item.rotation === 1 || item.rotation === 3 ? item.size[0] : item.size[1]
+
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                grid.setWalkableAt(
+                    item.gridPosition[0] + x,
+                    item.gridPosition[1] + y,
+                    false
+                )
+            }
+        }
+    })
+}
+
+updateGrid()
+
+console.log(findPath([3, 3], [4, 7]));
 
 /**
  * 
