e96714c1ef3cdb14d79d8e5434df4498fb475c5b
S.Zhang <zengeryounke@hotmail.com>
2024-03-03 12:58:27 -0800

    usegrid noted

--- client/src/hook/useGrid.jsx
+++ client/src/hook/useGrid.jsx
@@ -4,32 +4,37 @@ import * as THREE from "three"
 
 /**
  * 
- * create two normal use method with hook
+ * create two normal use methods with hook
  */
 
 export const useGrid = () => {
     const [map] = useAtom(mapAtom)
 
     /**
-     * convert vector3 to grid position form
-     * @returns 
+     * convert three js coordinate to the game grid coordinate
+     * @returns [x, y]
      */
     const vector3ToGrid = (vector3) => {
+        //gridDivision is the number of the cell in one three js unit
+        //the cell start from 1, so we need to use Math.floor to get the correct cell number
         return [
-            Math.floor(vector3.x / map.gridDivision),
-            Math.floor(vector3.z / map.gridDivision)
+            Math.floor(vector3.x * map.gridDivision),
+            Math.floor(vector3.z * map.gridDivision)
         ]
     }
 
     /**
-    * convert grid position to vector3
-    * @returns 
+    * convert game grid coordinate to the three js coordinate
+    * @param gridPosition [x, y], 
+    * @returns three vecotr3
     */
     const gridToVector3 = (gridPosition, width = 1, heigt = 1) => {
         return new THREE.Vector3(
-            width / map.gridDivision / 2 + gridPosition[0] / map.gridDivision,
+            width / map.gridDivision / 2 //width / divistion to get the one cell width in three js coordinate. divide  2 to center the location.
+            + gridPosition[0] / map.gridDivision,
             0,
-            heigt / map.gridDivision / 2 + gridPosition[1] / map.gridDivision
+            heigt / map.gridDivision / 2
+            + gridPosition[1] / map.gridDivision
         )
     }
 
